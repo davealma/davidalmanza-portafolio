191d535f3654c44515e45fe67c4f0b64c1ad1fa0
David <davidalmanza88@gmail.com>
2023-11-10 11:30:32 -0600

    Put chip badges below header

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,7 +12,7 @@ const NavBar = () => {
     }
     
     return(
-        <div className={`${isDark ? 'bg-darker' : 'bg-white'} w-full h-20 sticky top-0`}>
+        <div className={`${isDark ? 'bg-darker' : 'bg-white'} z-10 w-full h-20 sticky top-0`}>
             <div className="container mx-auto px-4 h-full">
                 <div className="flex justify-between items-center h-full">
                     <NavLinks isDark={isDark}/>
