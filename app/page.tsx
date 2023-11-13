1ea01eacba14ac1af8cea369d8d2b422d7f80245
David <davidalmanza88@gmail.com>
2023-11-13 12:57:21 -0600

    Structure home article content

--- app/page.tsx
+++ app/page.tsx
@@ -20,26 +20,32 @@ export default async function Home() {
   
   return (
     <ContentWrapper>
-      <img src={repoData.avatar_url} alt="Profile Image" className="rounded-full" width={100} height={100}/>
+      <div className='my-2'>
+        <img src={repoData.avatar_url} alt="Profile Image" className="rounded-full" width={100} height={100}/>
+      </div>
       <p>{repoData.bio}</p>
 
-      <article className='my-3 text-justify'>
-        I start working on a ecommerce tool with friends as my first job.
+      <article className='my-3 text-justify flex flex-col gap-4'>
+        <p>
+          I start working on a ecommerce tool with friends as my first job.
+        </p>
+
+        <p>
+          Throughout my career, I have successfully collaborated with both in-person and remote teams,
+          gaining valuable experience in adapting to diverse working environments.
+          Additionally, I have had the opportunity to work with clients from different parts of the world,
+          which has improved my abilities to perform under pressure and make decisions.
+        </p>
+
+        <p>
+          I have been working as a software engineer with different clients looking for solutions which satisfy the client.
+          I have the opportunity to work on Front End projects as Back End so I have the ability to understand the requirements on each side.
+          Also I love to learn and keep constantly reading and applying new and latest industry’s concepts and standards.
+        </p>
         
-        Throughout my career, I have successfully collaborated with both in-person and remote teams,
-        gaining valuable experience in adapting to diverse working environments.
-        Additionally, I have had the opportunity to work with clients from different parts of the world,
-        which has improved my abilities to perform under pressure and make decisions.
-
-        <br/>
-
-        I have been working as a software engineer with different clients looking for solutions which satisfy the client.
-        I have the opportunity to work on Front End projects as Back End so I have the ability to understand the requirements on each side.
-        Also I love to learn and keep constantly reading and applying new and latest industry’s concepts and standards.
-
-        
-        <br/>
-        As Hobbies I enjoy chess and some soccer, when I a not with my wife and son enjoying sun life.
+        <p>
+          As Hobbies I enjoy chess and some soccer, when I a not with my wife and son enjoying life.
+        </p>
       </article>
 
       <div className='my-3'>
